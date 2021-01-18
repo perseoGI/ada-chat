3b12e7ab1f3c8dc46d1800992df1ade0e398c447
petacreepers23 <javiermiguel4b@gmail.com>
2021-01-18 16:44:36 +0100

    Exceptions on file handling added

--- src/main.adb
+++ src/main.adb
@@ -1,33 +1,37 @@
 with Ada.Text_IO; use Ada.Text_IO;
-with RC4;
---with "./utils/utils";
 
 with Utils;
 
 procedure Main is
-	Input, Output: File_Type;
-	Line: String(1..10);
+   ConfigFile: File_Type;
 	Assert: Boolean;
 begin
-	--Create(File => Output,
-	--Mode => Out_File,
-	--Name => "adachat.cfg");
+    begin
+    --Opening of the file and error handling
+    Open(   File => ConfigFile,
+            Mode => In_File,
+            Name => "adachat.cfg");
+    exception
+        when others =>
+            Put_Line("No se ha encontrado adachat.cfg");
+            return;
+    end;
 
-	--Put_Line(Output, "1234567890");
+    --Main loop of reading
+    while not End_Of_File(ConfigFile) loop
+        declare
+            Line : String := Get_Line(ConfigFile);
+        begin
+            Assert := utils.Is_String_IpV4(Line);
+            Put_Line(Line);
+        end;
+    end loop;
 
-	--Close(Output);
-
-	Open(File => Input,
-	Mode => In_File,
-	Name => "adachat.cfg");
-
-	Line := Get_Line(Input);
-	Close(Input);
-	--Put_Line(Line);
-
-	Assert := utils.Is_String_IpV4("192.168.1.1");
-
-	utils.Is_String_IpV4_Test;
-
-	--Put_Line("Hola Mundo");
+    --Ending
+    Close(ConfigFile);
+exception
+    when others =>
+        if Is_Open(ConfigFile) then
+            Close (ConfigFile);
+        end if;
 end Main;
